f7c076594ff5258190dc76c289dd1196de150882
dominik-zeglen <flesz3@o2.pl>
2017-11-01 02:25:52 +0100

    Refactor code and file organization

--- src/public/js/components/content/ContainerChildren/index.js
+++ src/public/js/components/content/ContainerChildren/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import ContainerList from './ContainerChildren/ContainerList';
-import ObjectList from './ContainerChildren/ObjectList';
+import ContainerList from './ContainerList';
+import ObjectList from './ObjectList';
 
 class ContainerChildren extends React.Component {
   constructor(props) {

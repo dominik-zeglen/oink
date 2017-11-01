c122a7344ac56f3eb2bb8f3ae53ad5fcc8753700
dominik-zeglen <flesz3@o2.pl>
2017-11-01 02:43:35 +0100

    Remove unnecesary div

--- src/public/js/components/content/ManageCategoryList.js
+++ src/public/js/components/content/ManageCategoryList.js
@@ -88,26 +88,24 @@ class ManageCategoryList extends React.Component {
 
   render() {
     return <div>
-      <div>
-        <Breadcrumb fetchData={this.fetchData} breadcrumb={this.state.breadcrumb}/>
-        {this.state.loading ? <Loading/> : (
-          <div>
-            {this.props.match.params.id != '-1' && (
-              <ContainerProperties history={this.props.history}
-                                   currentContainer={this.state.currentContainer}
-                                   fetchData={this.fetchData} />
-            )}
-            <ContainerChildren fetchData={this.fetchData}
-                               categories={this.state.categories}
-                               objects={this.state.objects} />
-            <div className={'fixed-action-btn'} onClick={this.addContainer}>
-              <a className={'btn-floating btn-large waves-effect waves-light'}>
-                <i className={'material-icons'}>add</i>
-              </a>
-            </div>
+      <Breadcrumb fetchData={this.fetchData} breadcrumb={this.state.breadcrumb}/>
+      {this.state.loading ? <Loading/> : (
+        <div>
+          {this.props.match.params.id != '-1' && (
+            <ContainerProperties history={this.props.history}
+                                 currentContainer={this.state.currentContainer}
+                                 fetchData={this.fetchData}/>
+          )}
+          <ContainerChildren fetchData={this.fetchData}
+                             categories={this.state.categories}
+                             objects={this.state.objects}/>
+          <div className={'fixed-action-btn'} onClick={this.addContainer}>
+            <a className={'btn-floating btn-large waves-effect waves-light'}>
+              <i className={'material-icons'}>add</i>
+            </a>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>;
   }
 }

b1d4afab9b888d5dcd7176267afa717945a9f202
dominik-zeglen <flesz3@o2.pl>
2017-11-21 01:48:44 +0100

    Get rid of require()

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
 import * as express from 'express';
 import {readFile} from "fs";
 import * as http from 'http';
+import monk from 'monk';
 import * as logger from 'morgan';
 import {render} from 'pug';
-const db = require('monk')('mongodb://127.0.0.1:27017/oink');
 
 import {Oink} from './lib/Oink';
 
+const db = monk('mongodb://127.0.0.1:27017/oink');
 const app = express();
 const httpServer = new http.Server(app);
 const oink = new Oink(app, db);
 
 app.use(logger('dev'));
 app.get('/', (req, res) => {
-  readFile('./templates/index.pug', (e, f) => {
-    let content = db.get('objects')
+  readFile('./templates/index.pug', async (e, f) => {
+    const content = await db.get('objects')
       .findOne({_id: "5a0e19ec43ea61623a2be4ee"})
       .then((r) => oink.toObject(r));
     res.send(render(f.toString(), {

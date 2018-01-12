7b8681542cc2c2826817b8aecf9acbf16ee11243
dominik-zeglen <flesz3@o2.pl>
2018-01-12 01:05:34 +0100

    Add container management logic to core module

---
+++ dist/oink/core/container.js
@@ -0,0 +1,125 @@
+async function getContainer(id, db) {
+  return db.get('containers')
+    .findOne({ _id: id });
+}
+
+async function getContainerChildren(id, db) {
+  return db.get('containers')
+    .find({ parent_id: id });
+}
+
+async function getContainerAncestors(id, db) {
+  const breadcrumb = [];
+  const maxDepth = 10;
+
+  let counter = maxDepth;
+  let currentContainerId = id;
+  while (counter > 0 && currentContainerId && currentContainerId !== '-1') {
+    const r = await getContainer(currentContainerId, db);
+    if (breadcrumb.map(p => String(p._id)).includes(String(r._id))) {
+      return new Error('Cyclic inheritance chain');
+    }
+    breadcrumb.push(r);
+    currentContainerId = r.parent_id;
+    counter -= 1;
+  }
+  return breadcrumb.reverse();
+}
+
+async function addContainer(model, db) {
+  const params = {
+    created_at: +(new Date()),
+  };
+  return db.get('containers')
+    .insert(Object.assign(params, model));
+}
+
+async function updateContainer(id, params, db) {
+  return db.get('containers')
+    .update({ _id: id }, {
+      $set: {
+        description: params.description,
+        name: params.name,
+        visible: params.visible,
+        parent_id: params.parentId,
+      },
+    });
+}
+
+async function removeContainer(id, db) {
+  return db.get('containers')
+    .remove({ _id: id });
+}
+
+class Container {
+  constructor(id, db) {
+    this.init(id, db);
+  }
+
+  init(id, db) {
+    this.db = db;
+
+    getContainer(id, db)
+      .then((model) => {
+        if (model._id) {
+          this.id = model._id;
+          this.name = model.name;
+          this.description = model.description;
+          this.parentId = model.parent_id;
+          this.visibility = model.visible;
+          this.created_at = model.created_at;
+
+          return true;
+        }
+        return new Error('Error finding container');
+      });
+  }
+
+  getChildren() {
+    return getContainerChildren(this.id, this.db);
+  }
+
+  getAncestors() {
+    return getContainerAncestors(this.id, this.db);
+  }
+
+  async update(params) {
+    const result = await updateContainer(this.id, params, this.db)
+      .then(r => r.nMatched);
+
+    if (result === 1) {
+      Object.keys(params).forEach((key) => {
+        this[key] = params[key];
+      });
+      return true;
+    }
+    return new Error('Error updating container');
+  }
+
+  async remove() {
+    const result = await removeContainer(this.id, this.db)
+      .then(r => r.nRemoved);
+
+    if (result === 1) {
+      this.id = undefined;
+      this.name = undefined;
+      this.description = undefined;
+      this.parentId = undefined;
+      this.visibility = undefined;
+      this.created_at = undefined;
+
+      return true;
+    }
+    return new Error('Failed to remove container');
+  }
+}
+
+module.exports = {
+  getContainer,
+  getContainerChildren,
+  getContainerAncestors,
+  addContainer,
+  removeContainer,
+  updateContainer,
+  Container,
+};
---
+++ test/container.js
@@ -0,0 +1,79 @@
+const assert = require('assert');
+const faker = require('faker');
+const monk = require('monk');
+const _ = require('lodash');
+
+const container = require('../dist/oink/core/container');
+
+const dbPath = process.env.MONGODB_PATH || 'mongodb://127.0.0.1:27017/oink';
+const db = monk(dbPath);
+
+describe('Containers', () => {
+  const containerData = {
+    name: faker.name.firstName(0),
+    description: faker.random.word(),
+    parentId: '-1',
+    visible: true,
+  };
+
+  const containerChildData = {
+    name: faker.name.firstName(0),
+    description: faker.random.word(),
+    visible: true,
+  };
+
+  it('Create container', (done) => {
+    container.addContainer(containerData, db).then((inserted) => {
+      assert.notEqual(inserted._id, undefined);
+      containerData._id = inserted._id;
+      containerChildData.parent_id = inserted._id;
+      done();
+    }).catch(err => done(err));
+  });
+
+  it('Getting container', (done) => {
+    container.getContainer(containerData._id, db).then((fetched) => {
+      const keys = Object.keys(containerData);
+      keys.forEach((key, index) => {
+        if (key !== 'pass') {
+          assert(containerData[key], fetched[key]);
+        }
+        if (index === keys.length - 1) {
+          done();
+        }
+      });
+    }).catch(err => done(err));
+  });
+
+  it('Getting container children', async () => {
+    containerChildData._id = await container.addContainer(containerChildData, db)
+      .then(r => r._id);
+
+    return container.getContainerChildren(containerData._id, db).then((fetched) => {
+      assert.equal(fetched.map(o => String(o._id))
+        .includes(String(containerChildData._id)), true);
+    });
+  });
+
+  it('Getting container ancestors', async () => {
+    return container.getContainerAncestors(containerChildData._id, db).then((fetched) => {
+      assert.equal(fetched.map(o => String(o._id))
+        .includes(String(containerData._id)), true);
+      assert.equal(fetched.map(o => String(o._id))
+        .includes(String(containerChildData._id)), true);
+    });
+  });
+
+  it('Removing container', (done) => {
+    container.removeContainer(containerData._id, db)
+      .then((fetched) => {
+        assert.equal(fetched.result.ok, 1);
+        done();
+      });
+  });
+
+  after(() => {
+    container.removeContainer(containerChildData._id, db)
+      .then(() => db.close());
+  });
+});

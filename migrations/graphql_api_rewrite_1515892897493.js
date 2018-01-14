080f842bfdf08ebc117d2e1f43a0d3aaadf3e777
dominik-zeglen <flesz3@o2.pl>
2018-01-14 03:09:16 +0100

    Add migration

---
+++ migrations/graphql_api_rewrite_1515892897493.js
@@ -0,0 +1,29 @@
+const monk = require('monk');
+
+const dbPath = process.env.MONGODB_PATH || 'mongodb://127.0.0.1:27017/oink';
+const db = monk(dbPath);
+
+const collections = ['containers', 'modules', 'objects'];
+
+async function updateCollection(collection) {
+  await db.get(collection).insert({
+    parent_id: '1',
+    created_at: '2',
+  }).then(() => db.get(collection).find())
+    .then(containers => db.get(collection)
+      .remove({})
+      .then(() => db.get(collection)
+        .insert(containers.map((container) => {
+          const c = container;
+          c.parentId = c.parent_id;
+          c.createdAt = c.created_at;
+          delete c.parent_id;
+          delete c.created_at;
+          return c;
+        }))
+        .then(() => console.log(`Collection ${collection} updated`)))
+      .catch(e => console.log(e)))
+    .catch(e => console.log(e));
+}
+
+Promise.all(collections.map(updateCollection)).then(db.close);

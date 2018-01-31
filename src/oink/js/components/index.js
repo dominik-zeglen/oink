b457d3c876aacf55a8298884d610a2434e6b3f1d
dominik-zeglen <flesz3@o2.pl>
2018-02-01 00:52:27 +0100

    Reorganize containers

--- src/oink/js/components/index.js
+++ src/oink/js/components/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -12,15 +16,22 @@ import rootReducer from '../reducers';
 injectTapEventPlugin();
 
 const store = createStore(rootReducer);
+const apolloCache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: '/manage/graphql' }),
+  cache: apolloCache,
+});
 
 function renderApp() {
   ReactDOM.render(
     <Provider store={store}>
-      <MuiThemeProvider>
+      <ApolloProvider client={apolloClient}>
         <BrowserRouter>
-          <App favourites={[]} rootPath="/manage" />
+          <MuiThemeProvider>
+            <App favourites={[]} rootPath="/manage" />
+          </MuiThemeProvider>
         </BrowserRouter>
-      </MuiThemeProvider>
+      </ApolloProvider>
     </Provider>,
     document.getElementById('oink-app'),
   );
